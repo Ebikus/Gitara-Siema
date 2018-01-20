37cf9511e8e3c2f3d7bc8f6c6c65a506404cc04c
Ebikus <sebastian240698@gmail.com>
2018-01-20 11:42:47 +0100

    Dodawanie ziomki

---
+++ DajcieZyc/dodawanie/Dodawanie/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    float a;
+    float b;
+   cout << "Dodawanie: " << endl;
+   cout << "Podaj pierwszą liczbę: " << endl;
+   cin >> a;
+   cout << "Podaj drugą liczbę: " << endl;
+   cin >> b;
+   cout << "Wynik dodawania to: " << a+b << endl;
+
+   getchar();
+   getchar();
+   return 0;
+}
